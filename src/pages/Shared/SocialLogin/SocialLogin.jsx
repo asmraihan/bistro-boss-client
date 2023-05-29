c14db0b89ff260802da638340243319cd9725049
asmraihan <asmraihanctg14@gmail.com>
2023-05-29 11:59:18 +0600

    user info in backend

--- src/pages/Shared/SocialLogin/SocialLogin.jsx
+++ src/pages/Shared/SocialLogin/SocialLogin.jsx
@@ -13,7 +13,18 @@ const SocialLogin = () => {
             .then(result => {
                 const loggedinUser = result.user;
                 console.log('Logged in user', loggedinUser)
-                navigate(from, { replace: true })
+                const saveUser = { name: loggedinUser.displayName, email: loggedinUser.email }
+                fetch('http://localhost:5000/users', {
+                    method: 'POST',
+                    headers: {
+                        'content-type': 'application/json'
+                    },
+                    body: JSON.stringify(saveUser)
+                })
+                    .then(res => res.json())
+                    .then(() => {
+                        navigate(from, { replace: true })
+                    })
             })
     }
     return (
