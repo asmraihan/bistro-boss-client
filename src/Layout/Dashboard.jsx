447da09ca00bcee3307eeaf86009a32593149f9a
asmraihan <asmraihanctg14@gmail.com>
2023-05-30 12:36:51 +0600

    jwt cart and user

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,11 +1,14 @@
 import { FaBars, FaBook, FaCalendarAlt, FaHome, FaShoppingCart, FaUsers, FaUtensils, FaWallet } from "react-icons/fa";
 import { NavLink, Outlet } from "react-router-dom";
 import useCart from "../hooks/useCart";
+import useAdmin from "../hooks/useAdmin";
+
 
 const Dashboard = () => {
     const { cart } = useCart()
     // TODO: Check if user is admin for data form server
-    const isAdmin = true
+    // const isAdmin = true
+    const [isAdmin] = useAdmin()
     return (
         <div className="drawer drawer-mobile">
             <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
